372fa2373b966b99b51f766b717315958bdfee73
Sergio Borghese <s.borghese@netresults.it>
2023-07-18 23:09:08 +0200

    add script to build lib on bbot ub1604 builder

---
+++ lib/bbot-build-ub1604-64b.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp depspath.pri.bbot depspath.pri
+git clone https://gitlab.netresults.dev:10443/netresults/utils/scripts.git
+python3 scripts/wrappergen2/wrappergen.py -c nrpycpp_wrapper_conf.json
+qmake-5157
+make
+
